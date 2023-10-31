4672e3a8da2e28ea1bb14d785bc567327de556de
Maria Akhras <mariaakhras055@gmail.com>
2023-10-31 19:30:48 +0000

    employees solution

--- employee.py
+++ employee.py
@@ -4,28 +4,86 @@
 class Employee:
     def __init__(self, name):
         self.name = name
+        self.contract_type = None
+        self.hourly_wage = None
+        self.hours_worked = None
+        self.monthly_salary = None
+        self.commission_type = None
+        self.commission_value = 0
+        self.num_contracts = 0
+
+    def set_salary_contract(self, salary):
+        self.contract_type = "Salary"
+        self.monthly_salary = salary
+
+    def set_hourly_contract(self, wage, hours):
+        self.contract_type = "Hourly"
+        self.hourly_wage = wage
+        self.hours_worked = hours
+
+    def set_bonus_commission(self, bonus):
+        self.commission_type = "Bonus"
+        self.commission_value = bonus
+
+    def set_contract_commission(self, contracts, commission_per_contract):
+        self.commission_type = "Contract"
+        self.num_contracts = contracts
+        self.commission_value = commission_per_contract
+
+    def calculate_pay(self):
+        total_pay = 0
+
+        if self.contract_type == "Salary":
+            total_pay += self.monthly_salary
+            pay_details = f"{self.name} works on a monthly salary of {self.monthly_salary}."
+
+        elif self.contract_type == "Hourly":
+            total_pay += self.hourly_wage * self.hours_worked
+            pay_details = f"{self.name} works on a contract of {self.hours_worked} hours at {self.hourly_wage}/hour."
+
+        if self.commission_type == "Bonus":
+            total_pay += self.commission_value
+            pay_details += f" and receives a bonus commission of {self.commission_value}."
+
+        if self.commission_type == "Contract":
+            commission_earned = self.num_contracts * self.commission_value
+            total_pay += commission_earned
+            pay_details += f" and receives a commission for {self.num_contracts} contract(s) at {self.commission_value}/contract."
+
+        pay_details += f" Their total pay is {total_pay}."
+        return pay_details
 
     def get_pay(self):
-        pass
+        return self.calculate_pay()
 
     def __str__(self):
-        return self.name
+        return self.calculate_pay()
 
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
 billie = Employee('Billie')
+billie.set_salary_contract(4000)
 
 # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
 charlie = Employee('Charlie')
+charlie.set_hourly_contract(25, 100)
 
 # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
 renee = Employee('Renee')
+renee.set_salary_contract(3000)
+renee.set_contract_commission(4, 200)
 
 # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
 jan = Employee('Jan')
+jan.set_hourly_contract(25, 150)
+jan.set_contract_commission(3, 220)
 
 # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
 robbie = Employee('Robbie')
+robbie.set_salary_contract(2000)
+robbie.set_bonus_commission(1500)
 
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
 ariel = Employee('Ariel')
+ariel.set_hourly_contract(30, 120)
+ariel.set_bonus_commission(600)
